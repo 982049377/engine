89914997fe466844d89a95768a425597593adab0
王恒尊 <982049377@qq.com>
2017-03-29 11:30:22 +0800

    渲染类型出现问题，备份

--- src/core/resource.ts
+++ src/core/resource.ts
@@ -1,6 +1,9 @@
 namespace engine.RES {
+    var ImageJson = [
+        { name: "loading", url: "loading.png", width: 200, height: 200 },
+    ]
     var __cache = {}
-    export function getRes(name: string) {
+    export function getRes(name: string) : ImageResource{
 
         if (__cache[name]) {
             return __cache[name]
@@ -11,30 +14,53 @@ namespace engine.RES {
         }
     }
 
-    export function loadRes(name) {
+    export function loadRes(name) : ImageResource{
         var resource = getRes(name);
         resource.load();
+        return resource;
     }
 
     export function loadConfig() {
-       for(var index in __cache){
-           __cache[index].load();
-       }
+        for (var index in __cache) {
+            __cache[index].load();
+        }
     }
 
 
-    class ImageResource {
+    export function addImageJson(name: string, url: string, width: number, height: number) {
+        ImageJson.forEach(element => {
+            if (element.name == name)
+                return;
+        });
+        var tempElement = { name: name, url: url, width: width, height: height }
+        ImageJson.push(tempElement);
+    }
+
+    export class ImageResource {
         width = 0;
 
         height = 0;
 
         bitmapData: HTMLImageElement;
-
+        private static loadImage: HTMLImageElement;
         private url: string;
-        constructor(url: string) {
-            this.url = url;
-            this.bitmapData = document.createElement("img");
-            this.bitmapData.src = "loading";
+        constructor(name: string) {
+            ImageJson.forEach(element => {
+                if (element.name == name) {
+                    this.width = element.width;
+                    this.height = element.height;
+                    this.url = element.url;
+                }
+            });
+            // // this.url = url;
+            // this.bitmapData = document.createElement("img");
+            // ImageResource.loadImage = document.createElement("img");
+            // ImageResource.loadImage.src = "loading.png";
+            // console.log(ImageResource.loadImage.src);
+            // ImageResource.loadImage.onload = () => {
+            //     this.bitmapData = ImageResource.loadImage;
+            // }
+            // this.bitmapData = ImageResource.loadImage;
         }
         load() {
             var realResource = document.createElement("img");
